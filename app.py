d7ab7df92ba6adc0980995d7d80293f28a38284a
ARTIKA SINGH <106801844+artikasingh880@users.noreply.github.com>
2025-03-11 16:35:49 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+streamlit
+yfinance
+pandas
+numpy
+matplotlib
+scikit-learn
+tensorflow  # If using deep learning
+statsmodels  # If using ARIMA
+prophet  # If using Prophet
+echo "# financial-market-prediction" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/artikasingh880/financial-market-prediction.git
+git push -u origin main
