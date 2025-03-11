9963c685a0ab68f14bd36a47fa0573dfa1e96525
ARTIKA SINGH <106801844+artikasingh880@users.noreply.github.com>
2025-03-11 16:48:24 +0530

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-streamlit
-yfinance
-pandas
-numpy
-matplotlib
-scikit-learn
-tensorflow  # If using deep learning
-statsmodels  # If using ARIMA
-prophet  # If using Prophet
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/artikasingh880/financial-market-prediction.git
-git push -u origin main
+import streamlit as st
+
+st.title("Financial Market Prediction")
+st.write("Welcome to the Financial Market Prediction System!")
+
